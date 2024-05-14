ea55942aef80bb76751ee9332f60a3d41975eca1
Heyin Choi <97toki@naver.com>
2024-05-15 01:33:01 +0900

    240514 :: 초기 설정

---
+++ Mini_Project/src/com/stock/controller/MainPageController.java
@@ -0,0 +1,22 @@
+package com.stock.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class MainPageController implements Controller
+{
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception
+	{
+		ModelAndView mav = new ModelAndView();
+		
+		mav.setViewName("/WEB-INF/view/MainPage.jsp");
+		
+		return mav;
+	}
+}
+;
